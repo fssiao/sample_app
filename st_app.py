faa7188c54f66d8883d222bcf48d4acff32fa19b
mrhiwatig <151108023+mrhiwatig@users.noreply.github.com>
2023-11-17 13:52:27 +0800

    Add files via upload

---
+++ st_app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+def main():
+    st.set_page_config(
+        page_title="Customer response generator", page_icon=":bird:")
+
+    st.header("Customer response generator :bird:")
+    message = st.text_area("customer message")
+
+    if message:
+        st.write("Generating best practice message...")
+
+        result = generate_response(message)
+
+        st.info(result)
+
+
+if __name__ == '__main__':
+    main()
